93008878910328b624aeedeca0e144a61b810fcc
Yusuf Can Kesen <ykesen@prodyna.com>
2019-07-16 20:01:47 +0200

    v1.4.1 Jackson Finish

--- src/test/java/com/prodyna/training/jackson/JacksonTest.java
+++ src/test/java/com/prodyna/training/jackson/JacksonTest.java
@@ -1,69 +1,101 @@
 package com.prodyna.training.jackson;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+import java.util.List;
 import org.junit.Test;
 
 
 public class JacksonTest {
 
 
-    @Test
-    public void createPersonAndMarshalToFileTest() {
+  @Test
+  public void createPersonAndMarshalToFileTest() {
 
-        try {
+    try {
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+      Person person = new Person("Mr", "Leonardo di Caprio",
+          Gender.MALE, 45, "1988-02-01");
+      objectMapper.writeValue(new File("target/person.json"), person);
 
 
-        } catch (Exception e) {
-
-        }
+    } catch (Exception e) {
 
     }
 
-    @Test
-    public void unMarshalToPersonFromStringTest() {
-        try {
+  }
 
+  @Test
+  public void unMarshalToPersonFromStringTest() {
 
+    String personJson = "{\"title\":\"Mr\",\"fullName\":\"Leonardo di Caprio\",\"gender\":\"MALE\",\"age\":45,\"birthday\":\"1988-02-01\"}";
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      Person person = objectMapper.readValue(personJson, Person.class);
+      assertThat(person, is(notNullValue()));
+      assertThat(person.getTitle(), is("Mr"));
 
-        } catch (Exception e) {
+    } catch (Exception e) {
 
-        }
     }
+  }
 
-    @Test
-    public void unMarshalFromFileToPersonTest() {
-        try {
+  @Test
+  public void unMarshalFromFileToPersonTest() {
+    try {
 
+      ObjectMapper objectMapper = new ObjectMapper();
+      Person person = objectMapper.readValue(new File("person.json"), Person.class);
+      assertThat(person, is(notNullValue()));
+      assertThat(person.getTitle(), is("Mr"));
 
-        } catch (Exception e) {
+    } catch (Exception e) {
 
-        }
     }
+  }
 
-    @Test
-    public void readFileAsJsonNodeTest() {
-        try {
+  @Test
+  public void readFileAsJsonNodeTest() {
+    try {
 
+      ObjectMapper objectMapper = new ObjectMapper();
+      //no type needed
+      JsonNode personNode = objectMapper.readTree(new File("person.json"));
+      assertThat(personNode, is(notNullValue()));
+      assertThat(personNode.get("title"), is("Mr"));
 
+    } catch (Exception e) {
 
-        } catch (Exception e) {
-
-        }
     }
+  }
 
-    @Test
-    public void readStringAsPersonListTest() {
+  @Test
+  public void readStringAsPersonListTest() {
 
-        String jsonArray =
-            "[{ \"title\" : \"mr\", \"fullName\" : \"Neo\" }, { \"title\" : \"mrs\", \"fullName\" : \"Alice\" }]";
+    String jsonArray =
+        "[{ \"title\" : \"mr\", \"fullName\" : \"Neo\" }, { \"title\" : \"mrs\", \"fullName\" : \"Alice\" }]";
 
-        try {
+    try {
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      List<Person> listPerson = objectMapper.readValue(jsonArray, new TypeReference<List<Person>>(){});
+
+      assertThat(listPerson.size(), is(2));
+      assertThat(listPerson.get(0).getFullName(), is("Neo"));
 
 
+    } catch (Exception e) {
 
-
-        } catch (Exception e) {
-
-        }
     }
+  }
 
 }
